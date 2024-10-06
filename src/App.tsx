d57f3d52017ebcc9d80a4850ed9a76fb26aea917
“cybersparkx” <lucammontanari@gmail.com>
2024-10-06 08:26:45 +0200

    started front-end

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
+import ButtonCard from "./buttonCard"; // Import the buttonCard component
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -21,40 +22,51 @@ function App() {
 
   return (
     <div className="container">
-      <h1>Meridian Focus</h1>
+      <h1 className="text-5xl font-bold my-8">Meridian Focus</h1>
+      <div className="w-full h-auto grid grid-cols-3 grid-rows-2" id="gridWrap">
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div id="toolBtnsContainer1" className="bg-blue-500">
+        </div>
+        <div id="toolBtnsContainer2" className="bg-blue-500">
+        </div>
+        <div id="toolBtnsContainer3" className="bg-blue-500">
+        </div>
+        <div className="midWrapper">
+          <div className="flex justify-center">
+            <ButtonCard title="Tauri" icon={reactLogo} />
+            <ButtonCard title="Tauri" icon={reactLogo} />
+            <ButtonCard title="Tauri" icon={reactLogo} />
+            <ButtonCard title="Tauri" icon={reactLogo} />
+          </div>
+          <div className="flex justify-center">
+            <ButtonCard title="Tauri" icon={reactLogo} />
+            <ButtonCard title="Tauri" icon={reactLogo} />
+            <ButtonCard title="Tauri" icon={reactLogo} />
+            <ButtonCard title="Tauri" icon={reactLogo} />
+          </div>
+        </div>
+
+
+        {/* <form
+          className="row"
+          onSubmit={(e) => {
+            e.preventDefault();
+            greet();
+          }}
+        >
+          <input
+            id="greet-input"
+            onChange={(e) => setName(e.currentTarget.value)}
+            placeholder="Enter a name..."
+          />
+          <button type="submit">Greet</button>
+        </form>
+
+        <p>{greetMsg}</p> */}
       </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/buttonCard.tsx
@@ -0,0 +1,21 @@
+// src/buttonCard.tsx
+import React from 'react';
+import "./App.css";
+
+interface ButtonCardProps {
+  title: string;
+  icon: string;
+}
+
+const ButtonCard: React.FC<ButtonCardProps> = ({ title, icon }) => {
+  return (
+    <div id='buttonCardContainer' className="flex flex-col justify-center rounded-md px-[10px] pt-[10px] w-[100px] h-[120px]
+    m-[10px]" style={{ border: '1px solid gray', transition: 'all 0.2s linear' }}>
+      <img src={icon} alt={title} />
+      <div className='my-[5px]'></div>
+      <h3>{title}</h3>
+    </div>
+  );
+};
+
+export default ButtonCard;
